91cfbceff04248d7dd1d57792d875317f12e6b08
aysha9846 <130770912+aysha9846@users.noreply.github.com>
2023-09-30 15:45:24 +0530

    Add files via upload

---
+++ sumandavg.py
@@ -0,0 +1,8 @@
+num=int(input("how many numbers ?"))
+total_sum=0
+for n in range(num):
+    number=float(input("enter any numbers"))
+    total_sum += number
+avg=total_sum/num
+print(avg)
+        
