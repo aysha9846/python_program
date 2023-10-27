67976e8a17eca05b2011b7ec4103b32f55d507a2
aysha9846 <130770912+aysha9846@users.noreply.github.com>
2023-10-27 16:30:11 +0530

    Add files via upload

---
+++ dict_asc_des.py
@@ -0,0 +1,6 @@
+d={1:2,3:4,4:3,2:1,0:0,3:5}
+print("original dictionary",d)
+sorted_d=sorted(d.items())
+print("dictionary in ascending order : ",sorted)
+sorted_d_reverse=sorted(d.item(),reverse=True)
+print("dictionary in desennding order : ",sorted_d.reverse)
