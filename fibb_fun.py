a27beb83b7e109daa1d4e2c881cdaeb19140d38d
aysha9846 <130770912+aysha9846@users.noreply.github.com>
2023-10-27 23:11:04 +0530

    Add files via upload

---
+++ fibb_fun.py
@@ -0,0 +1,14 @@
+def fib(i):
+    a=0
+    b=1
+    print(a)
+    print(b)
+    for i in range(j-2):
+        z=a+b
+        print(z)
+        a=b
+        b=z
+j=int(input("enter the number of terms:"))
+fib(j)
+    
+        
