7a24807f66e92d1b5755f29b70c9aa6ed3966612
aysha9846 <130770912+aysha9846@users.noreply.github.com>
2023-11-03 16:17:19 +0530

    Add files via upload

---
+++ pyramid_star.py
@@ -0,0 +1,11 @@
+num=int(input("enter the number of rows"))
+for i in range(0,num):
+    for j in range(0,i+1):
+        print("*",end=' ')
+    print("\r")
+for i in range(num,0,-1):
+    for j in range(0,i-1):
+        print("*",end=' ')
+    print("\r")
+    
+        
